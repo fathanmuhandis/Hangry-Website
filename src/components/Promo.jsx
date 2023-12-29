b73341903fe80b057b18266cec16445789f6a62c
fathanmuhandis <m.fathan11@ui.ac.id>
2023-12-29 11:41:23 +0700

    revision promo

--- src/components/Promo.jsx
+++ src/components/Promo.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaCheck } from "react-icons/fa";
 
 const Promo = () => {
     return (
@@ -10,8 +11,22 @@ const Promo = () => {
                     <p className='font-medium text-2xl text-[#707070]'>pembelian 20 porsi menu apa aja. Cocok untuk</p>
                     <p className='font-medium text-2xl text-[#707070]'>hidangan segala acara!</p>
                 </div>
-                <div>
-                    <h1 className='font-bold text-5xl text-[#303030] py-2'>Makin rame, makin hemat!</h1>
+                <div className='space-y-2'>
+                    <div className='flex items-center space-x-4'>
+                        <FaCheck className='text-sauce text-2xl' />
+                        <p className="text-2xl font-bold">Beragam pilihan menu</p>
+                    </div>
+                    <div className='flex items-center space-x-4'>
+                        <FaCheck className='text-sauce text-2xl' />
+                        <p className="text-2xl font-bold">Semua menu diskon 20%</p>
+                    </div>
+                    <div className='flex items-center space-x-4'>
+                        <FaCheck className='text-sauce text-2xl' />
+                        <div>
+                            <p className="text-2xl font-bold">Gratis biaya kirim</p>
+                            <p className="text-sm font-bold text-[#707070]">*Syarat & ketentuan berlaku</p>
+                        </div>
+                    </div>
                 </div>
             </div>
 
